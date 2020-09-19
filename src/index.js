ed03129c6dee7df393ca1bf010a44037a71dfb18
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-09-19 20:30:57 +0100

    Added Icon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const iconPath = path.join(__dirname, "icon.png");
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -18,7 +20,8 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-    }
+    },
+    icon: iconPath
   });
 
   // and load the index.html of the app.
